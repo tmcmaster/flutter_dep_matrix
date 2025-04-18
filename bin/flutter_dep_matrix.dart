bab13200479299775b866167b9d39d7a8b4095be
Tim McMaster <tim@mcmaster.id.au>
2025-04-18 10:06:55 +1000

    initialised the project

---
+++ bin/flutter_dep_matrix.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_dep_matrix/flutter_dep_matrix.dart';
+
+void main(List<String> args) {
+  run(args);
+}
---
+++ lib/flutter_dep_matrix.dart
@@ -0,0 +1,9 @@
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+run(args){
+  print('Hello there');
+}
